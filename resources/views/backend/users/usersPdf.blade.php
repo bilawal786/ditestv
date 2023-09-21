3be5fb895f8a17edca478c341befddc53b7f46f2
haseebrajput <haseebrajput503@gmail.com>
2023-09-21 12:26:13 +0500

    working on website

---
+++ resources/views/backend/users/usersPdf.blade.php
@@ -0,0 +1,102 @@
+{{--<!DOCTYPE html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="utf-8">--}}
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+{{--    <title>CodingCrust | Invoice Print</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--<div class="container" style="width: 1600px;border: 1px solid black;margin: 0 40px;" >--}}
+{{--    <div style="display: flex;">--}}
+{{--    <div style="padding-right: 40px;width: 500px">--}}
+{{--        <img src="{{asset('images/loding.png')}}" width="180" height="80">--}}
+{{--    </div>--}}
+{{--    <div style="width: 400px;text-align: center">--}}
+{{--        <h1>All Users</h1>--}}
+{{--    </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+{{--</body>--}}
+
+{{--@php--}}
+{{--    $users = \App\Models\User::where('role',1)->get();--}}
+{{--@endphp--}}
+
+{{--<body>--}}
+{{--<div class="container_fluid">--}}
+{{--    <div class="row">--}}
+{{--        <div class="col-12 table-responsive">--}}
+{{--            <table class="table table-striped">--}}
+{{--                <thead>--}}
+{{--                <tr>--}}
+{{--                    <th style="padding-right: 100px;">First Name</th>--}}
+{{--                    <th style="padding-right: 100px;">Last Name</th>--}}
+{{--                    <th style="padding-right: 100px;text-align: start;">email</th>--}}
+{{--                    <th style="padding-left: 30px;padding-right: 100px;">Phone</th>--}}
+{{--                    <th style="padding-right: 100px;">resident</th>--}}
+{{--                    <th style="padding-right: 100px;padding-left: 30px;">City</th>--}}
+{{--                    <th style="padding-right: 100px;padding-left: 30px;">Province</th>--}}
+{{--                    <th style="padding-right: 100px;padding-left: 30px;">Postal Code</th>--}}
+{{--                    <th style="padding-left: 30px;">Postal Code</th>--}}
+{{--                </tr>--}}
+{{--                </thead>--}}
+{{--                <tbody>--}}
+{{--                @foreach ($users as $user)--}}
+{{--                    <tr>--}}
+{{--                        <td class="m-5">{{$user->first_name}}</td>--}}
+{{--                        <td class="">{{$user->last_name}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 5px; ">{{$user->email}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->phone_number}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 5px;">{{$user->resident}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->city}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->province}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->postal_code}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->village}}</td>--}}
+{{--                    </tr>--}}
+{{--                @endforeach--}}
+{{--                </tbody>--}}
+{{--            </table>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <br>--}}
+{{--    <div class="row">--}}
+{{--        <div class="col-12 table-responsive">--}}
+{{--            <table class="table table-striped">--}}
+{{--                <thead>--}}
+{{--                <tr>--}}
+{{--                    <th style="padding-right: 100px;">First Name</th>--}}
+{{--                    <th style="padding-right: 100px;">Last Name</th>--}}
+{{--                    <th style="padding-right: 100px;text-align: start;">email</th>--}}
+{{--                    <th style="padding-left: 30px;padding-right: 100px;">Phone</th>--}}
+{{--                    <th style="padding-right: 100px;">resident</th>--}}
+{{--                    <th style="padding-right: 100px;padding-left: 30px;">City</th>--}}
+{{--                    <th style="padding-right: 100px;padding-left: 30px;">Province</th>--}}
+{{--                    <th style="padding-right: 100px;padding-left: 30px;">Postal Code</th>--}}
+{{--                    <th style="padding-left: 30px;">Postal Code</th>--}}
+
+
+{{--                </tr>--}}
+{{--                </thead>--}}
+{{--                <tbody>--}}
+{{--                @foreach ($users as $user)--}}
+{{--                    <tr>--}}
+{{--                        <td class="m-5">{{$user->first_name}}</td>--}}
+{{--                        <td class="">{{$user->last_name}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 5px; ">{{$user->email}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->phone_number}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 5px;">{{$user->resident}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->city}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->province}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->postal_code}}</td>--}}
+{{--                        <td class="m-5" style="padding-left: 30px;">{{$user->village}}</td>--}}
+{{--                    </tr>--}}
+{{--                @endforeach--}}
+{{--                </tbody>--}}
+{{--            </table>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+{{--</body>--}}
+{{--</html>--}}
