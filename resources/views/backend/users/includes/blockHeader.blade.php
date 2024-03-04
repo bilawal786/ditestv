d8b6e48e56190bf1005247582f493eade133a073
Haseeb <haseeb@codingcrust.com>
2024-03-04 18:36:10 +0500

    Design optmize

--- resources/views/backend/users/includes/blockHeader.blade.php
+++ resources/views/backend/users/includes/blockHeader.blade.php
@@ -1,31 +1,28 @@
 <div class="block-header">
     <div class="row clearfix">
         <div class="col-md-12 col-sm-12 text-right">
-            <a href="{{route('users.create')}}" class="btn btn-sm btn-primary btn-round" style="margin-bottom: 10px;">
+            <a href="{{route('users.create')}}" class="btn btn-sm btn-primary " >
                 <i class="fa fa-plus"></i> Crea nuovo cliente</a>
-            <a href="{{ route('userPdf') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;">
+            <a href="{{ route('userPdf') }}" class="btn btn-sm btn-warning " >
                 <i class="fa fa-file-pdf-o"></i> Salva clienti PDF</a>
 
-            <a href="{{ route('expireduser') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;">
+            <a href="{{ route('expireduser') }}" class="btn btn-sm btn-dark " >
                 <i class="fa fa-file-pdf-o"></i> PDF Clienti scaduti</a>
 
-            <a href="{{ route('expiredDays') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;">
+            <a href="{{ route('expiredDays') }}" class="btn btn-sm btn-danger " >
                 <i class="fa fa-file-pdf-o"></i> Pdf scaduto di sette giorni</a>
 
-            <a href="{{ route('export.data') }}" class="btn btn-sm btn-danger btn-round " style="margin-bottom: 10px;">
+            <a href="{{ route('export.data') }}" class="btn btn-sm btn-success  " >
                 <i class="fa fa-file-excel-o"></i> Esporta tutti i clienti</a>
-            <a href="{{ route('expired.data') }}" style="margin-right: 5px; margin-bottom: 10px;"
-               class="btn btn-sm btn-danger btn-round">
+            <a href="{{ route('expired.data') }}"
+               class="btn btn-sm btn-secondary ">
                 <i class="fa fa-file-excel-o"></i> Esporta Utenti Scaduti
             </a>
 {{--            <a href="{{ route('expiredSevenDays') }}" style="margin-right: 5px; margin-bottom: 10px;"--}}
 {{--               class="btn btn-sm btn-danger btn-round" title="">--}}
 {{--                Sette giorni scaduti--}}
 {{--            </a>--}}
-            <ul class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}"><i class="icon-home"></i></a></li>
-                <li class="breadcrumb-item active"><a href="{{route('users.index')}}">Cliente</a></li>
-            </ul>
+
         </div>
     </div>
 </div>
