aefda66489574ecb42200a3600bafa0b4299e85a
Haseeb <haseeb@codingcrust.com>
2024-03-04 18:25:40 +0500

    Design optmize

--- resources/views/backend/users/includes/blockHeader.blade.php
+++ resources/views/backend/users/includes/blockHeader.blade.php
@@ -1,21 +1,21 @@
 <div class="block-header">
     <div class="row clearfix">
         <div class="col-md-12 col-sm-12 text-right">
-            <a href="{{route('users.create')}}" class="btn btn-sm btn-primary btn-round" style="margin-bottom: 10px;"
-               title=""><i class="fa fa-plus-square"></i> Crea nuovo cliente</a>
-            <a href="{{ route('userPdf') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;"
-               title=""><i class="fa fa-file-pdf-o"></i> Salva clienti PDF</a>
+            <a href="{{route('users.create')}}" class="btn btn-sm btn-primary btn-round" style="margin-bottom: 10px;">
+                <i class="fa fa-plus"></i> Crea nuovo cliente</a>
+            <a href="{{ route('userPdf') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;">
+                <i class="fa fa-file-pdf-o"></i> Salva clienti PDF</a>
 
-            <a href="{{ route('expireduser') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;"
-               title=""><i class="fa fa-file-pdf-o"></i> PDF Clienti scaduti</a>
+            <a href="{{ route('expireduser') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;">
+                <i class="fa fa-file-pdf-o"></i> PDF Clienti scaduti</a>
 
-            <a href="{{ route('expiredDays') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;"
-               title=""><i class="fa fa-file-pdf-o"></i> Pdf scaduto di sette giorni</a>
+            <a href="{{ route('expiredDays') }}" class="btn btn-sm btn-success btn-round" style="margin-bottom: 10px;">
+                <i class="fa fa-file-pdf-o"></i> Pdf scaduto di sette giorni</a>
 
-            <a href="{{ route('export.data') }}" class="btn btn-sm btn-danger btn-round  " style="margin-bottom: 10px;"
-               title=""><i class="fa fa-file-excel-o"></i> Esporta tutti i clienti</a>
+            <a href="{{ route('export.data') }}" class="btn btn-sm btn-danger btn-round " style="margin-bottom: 10px;">
+                <i class="fa fa-file-excel-o"></i> Esporta tutti i clienti</a>
             <a href="{{ route('expired.data') }}" style="margin-right: 5px; margin-bottom: 10px;"
-               class="btn btn-sm btn-danger btn-round" title="">
+               class="btn btn-sm btn-danger btn-round">
                 <i class="fa fa-file-excel-o"></i> Esporta Utenti Scaduti
             </a>
 {{--            <a href="{{ route('expiredSevenDays') }}" style="margin-right: 5px; margin-bottom: 10px;"--}}
